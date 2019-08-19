713c0244bcfdb9040e419dc2f2ce4970e1852e9f
Travis Horn <travis@travishorn.com>
2019-08-19 10:36:45 -0500

    Fetch posts in Vue app

---
+++ src/components/RecentPostsListing.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ posts }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecentPostsListing",
+  data() {
+    return {
+      posts: []
+    };
+  },
+  mounted() {
+    fetch("/.netlify/functions/posts")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.posts = data;
+      });
+  }
+};
+</script>
